0a43d6d8bcbb0f794902c420c489a21840abfe4d
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-02-04 18:09:56 +0100

    fix routing

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -6,9 +6,9 @@ const Tables = () => (
   <div className={styles.component}>
     <h2>Tables view</h2>
     <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>New Booking</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/xyz`}>Booking xyz</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Booking id</Link>
     <Link to={`${process.env.PUBLIC_URL}/tables/event/new`}>New Event</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/xxx`}>Event xxx</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Event id</Link>
   </div>
 );
 
